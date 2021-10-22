997cdae7a8462d463125042fa50d3d10410ddf9d
zalogarciam <gegarciam95@gmail.com>
2021-10-22 17:44:01 -0500

    example of view

--- panel/views.py
+++ panel/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    name = 'hola'
+    numbers = [1, 2, 3 , 4, 5]
     return render(request, 'panel/home.html', locals())
